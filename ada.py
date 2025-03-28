1588bef1425b07098c47443b0d8cb624be1c3523
lourdesmonsalvo <ljmonsalvo@mail.austral.edu.ar>
2025-03-28 11:34:32 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,12 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    first_name = "AdA"
+    last_name = "LoVeLAce"
+    full_name = f"{first_name} {last_name}"
+    space = "   " 
+
+    print (full_name.lower())
+    print (full_name.title())
+    print(full_name.upper())
+    print (space + full_name.lower())
